e52b9c87585d5ecdc2739bcc8bac0bd62c951d22
APolihron <apolihron@gmail.com>
2018-09-24 13:40:16 +0300

    First commit

---
+++ app/main.js
@@ -0,0 +1,30 @@
+const electron = require('electron');
+const { shell, app, BrowserWindow } = electron;
+const HOMEPAGE = 'https://www.udemy.com/'
+
+let mainWindow;
+
+app.on('ready', () => {
+    window = new BrowserWindow({
+        width: 1200,
+        height: 900,
+        webPreferences: {
+          nodeIntegration: false
+        }
+    });
+    window.setMenuBarVisibility(true);
+    window.loadURL(HOMEPAGE);
+
+    window.webContents.on('will-navigate', (ev, url) => {
+        parts = url.split('/');
+        if (parts[0] + '//' + parts[2] != HOMEPAGE) {
+            ev.preventDefault();
+            shell.openExternal(url);
+        };
+    });
+
+    window.on('closed', () => {
+        window = null;
+    });
+});
+
